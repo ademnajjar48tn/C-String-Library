363761271defc97066ee438278fd1fbcf4c571e7
ademnajjar48 <cctestingportugal@gmail.com>
2024-07-08 20:28:25 +0100

    Create function to create a new string called string_new that accepts an integer for the size of the string

--- Stringlib.h
+++ Stringlib.h
@@ -1,3 +1,16 @@
 #include <string.h>
 
 typedef char* string;
+
+string string_new(int size = 0){
+	if(size <= 0) {
+
+		string result = nullptr;
+		
+		return result;
+	}
+
+	string result = malloc(sizeof(char) * );
+
+	return result;
+}
